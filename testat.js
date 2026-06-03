807f89cffa18e14893f6f913ba8834d0a7c0fd0c
Hubert Bubert <hubertbubert@MacBook-Air-von-Hubert.local>
2026-06-03 15:34:53 +0200

    Fix: bind click listeners to buttons in testat.js

---
+++ testat.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("Projekte");
+button.addEventListener('click', () => {
+    window.location.href = 'Projekte.html'
+});
+
+const button2 = document.getElementById("Stats");
+button.addEventListener('click', () => {
+    window.location.href = 'Stats.html'
+});
